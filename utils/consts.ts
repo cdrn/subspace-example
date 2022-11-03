4fe609e696d8bb8a991e990681a169f3d88dc9be
Chris <chrisdoran@protonmail.com>
2022-11-03 21:20:21 +0100

    chore: connect to node, add loading spinner, structuring

---
+++ utils/consts.ts
@@ -0,0 +1,3 @@
+// Use the eu-0 gemini 2a node as default. We can let the user add their own node later.
+export const DEFAULT_NODE = "eu-0.gemini-2a.subspace.network" 
+export const DEFAULT_NODE_WEBSOCKET_URI = `wss://${DEFAULT_NODE}/ws`;
